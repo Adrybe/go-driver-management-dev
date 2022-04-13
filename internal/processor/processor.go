d8f373edd2ba9b21c341dcf24e7e6de2ef3347ff
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 17:17:47 -0300

    change query func

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -36,11 +36,8 @@ func CreateAdmin(w http.ResponseWriter, r *http.Request) {
 	var admin dto.Admin
 	err = json.NewDecoder(r.Body).Decode(&admin)
 	id := uuid.New().String()
-	errr := client.QueryRow(`INSERT INTO public.administrators(id, username, adminpassword, authorized)
+	client.Exec(`INSERT INTO public.administrators(id, username, adminpassword, authorized)
     VALUES($1, $2, $3, $4);`, id, admin.UserName, admin.Password, "PENDING")
-	if errr != nil {
-		log.Fatal(http.StatusInternalServerError)
-	}
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(dto.Response{Description: "Admin creado"})
