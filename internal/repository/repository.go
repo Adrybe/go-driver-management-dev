daa64a9419d1f2df133decb221b9657fae6f8ee9
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 17:42:13 -0300

    enable ssl db mode

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewRepository() (*sql.DB, error) {
-	connStr := "user=rdqoeghpgmnegr password=aaa1b4f4ec51dab764cb5869ecf92ac79391a360712f3ecebec93400534b7289 host=ec2-52-73-155-171.compute-1.amazonaws.com dbname=d7tsslec2uqb97  sslmode=disable"
+	connStr := "user=rdqoeghpgmnegr password=aaa1b4f4ec51dab764cb5869ecf92ac79391a360712f3ecebec93400534b7289 host=ec2-52-73-155-171.compute-1.amazonaws.com dbname=d7tsslec2uqb97  sslmode=enable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Error open connection: %s", err)
