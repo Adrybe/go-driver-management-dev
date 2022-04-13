66d9efb9703af43ae6efc11de679bd2aebcee9ff
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 06:36:26 -0300

    change the postgress connection

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewRepository() (*sql.DB, error) {
-	connStr := "user=rdqoeghpgmnegr dbname=d7tsslec2uqb97 host=ec2-52-73-155-171.compute-1.amazonaws.com password=aaa1b4f4ec51dab764cb5869ecf92ac79391a360712f3ecebec93400534b7289 sslmode=disable"
+	connStr := "user=rdqoeghpgmnegr password=aaa1b4f4ec51dab764cb5869ecf92ac79391a360712f3ecebec93400534b7289 host=ec2-52-73-155-171.compute-1.amazonaws.com dbname=d7tsslec2uqb97  sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
