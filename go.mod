681b41eda52e01d47bf676d4cf860a3d90fbb9f4
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 21:00:14 -0300

    added getAdmin

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ go 1.16
 require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
-	github.com/lib/pq v1.10.5 // indirect
+	github.com/lib/pq v1.10.5
 )
--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Adrybe/go-driver-management-dev/internal/repository"
 	"github.com/Adrybe/go-driver-management-dev/pkg/dto"
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -20,6 +21,13 @@ type (
 		w http.ResponseWriter
 		r *http.Request
 	}
+
+	Admin struct {
+		Id         string
+		UserName   string
+		Password   string
+		Authorized string
+	}
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -28,22 +36,72 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateAdmin(w http.ResponseWriter, r *http.Request) {
+
 	w.Header().Set("Content-Type", "application/json")
 	db, err := repository.NewRepository()
 	if err != nil {
 		log.Fatalf("error geting the DB: %s", err)
 	}
+
 	var admin dto.Admin
 	err = json.NewDecoder(r.Body).Decode(&admin)
+	if err != nil {
+		log.Fatalf("error parsing request body: %+v", err)
+	}
+
 	id := uuid.New().String()
+
 	result, err := db.Exec(`INSERT INTO public.administrators(id, username, adminpassword, authorized)
     VALUES($1, $2, $3, $4);`, id, admin.UserName, admin.Password, "PENDING")
 	if err != nil {
 		log.Fatalf("error inserting a new admin: %s", err)
 	}
+
 	log.Printf("%+v", result)
 
 	defer db.Close()
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(dto.Response{Description: "Admin creado"})
+}
+
+func GetAdmin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	db, err := repository.NewRepository()
+	if err != nil {
+		log.Fatalf("error geting the DB: %s", err)
+	}
+
+	params := mux.Vars(r)
+
+	userName := params["user_name"]
+
+	if err != nil {
+		log.Fatalf("error parsing request header: %+v", err)
+	}
+
+	result, err := db.Query(`SELECT *
+		FROM public.administrators
+		WHERE username = $1;`, userName)
+	if err != nil {
+		log.Fatalf("error fetching admin: %s", err)
+	}
+	var admin Admin
+	for result.Next() {
+		var id, username, adminpassword, authorized string
+		result.Scan(&id, &username, &adminpassword, &authorized)
+		admin = Admin{
+			Id:         id,
+			UserName:   username,
+			Password:   adminpassword,
+			Authorized: authorized,
+		}
+		log.Printf("el administrador encontrado es %+v", admin)
+
+	}
+
+	log.Printf("%+v", result)
+
+	defer db.Close()
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(admin)
 }
