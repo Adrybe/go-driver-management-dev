b299b1e474b3fd4950b9c1e7c7ef906b7dc3d57c
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-14 20:36:38 -0300

    update database credentials

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewRepository() (*sql.DB, error) {
-	connStr := "user=rdqoeghpgmnegr password=aaa1b4f4ec51dab764cb5869ecf92ac79391a360712f3ecebec93400534b7289 host=ec2-52-73-155-171.compute-1.amazonaws.com dbname=d7tsslec2uqb97  sslmode=require"
+	connStr := "user=nugxkoeirydlkl password=b6bcdbd33f115a380de9e9c0f8e8bd9042320e5d85e16c975e6f5eafef52d41e host=ec2-3-217-113-25.compute-1.amazonaws.com dbname=d80vbmv0622q6b  sslmode=require"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Error open connection: %s", err)
